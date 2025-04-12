b3bf1d215fe7880cebae0f492afff473ac660e74
hernan <hernanrichasse@gmail.com>
2025-04-12 13:28:42 -0400

    ultimo commit

---
+++ src/main/java/com/example/biblioteca2/controller/LibroController.java
@@ -0,0 +1,15 @@
+package com.example.biblioteca2.controller;
+
+import com.example.biblioteca2.model.Libro;
+import com.example.biblioteca2.service.LibroService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.controller;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+public class LibroController {
+
+}
