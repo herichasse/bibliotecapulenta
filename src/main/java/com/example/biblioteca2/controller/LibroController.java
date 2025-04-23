4f46c66c747eb3d2622ebc1a1ffd971ccd948e34
hernan <hernanrichasse@gmail.com>
2025-04-23 19:45:44 -0400

    actualizacion de codigo

--- src/main/java/com/example/biblioteca2/controller/LibroController.java
+++ src/main/java/com/example/biblioteca2/controller/LibroController.java
@@ -7,11 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+
 
 
 @RestController
@@ -38,6 +45,32 @@ public class LibroController {
          return libroService.buscarLibroPorIsbn(isbn);
      }
      
+     @GetMapping("/autor/{autor}")
+    public Libro buscarLibroPorAutor(@PathVariable String autor) {
+         return libroService.buscarLibroPorAutor(autor);
+    }
+
+    @PostMapping
+    public Libro guardarLibro(@RequestBody Libro libro) {    
+        return libroService.guardarLibro(libro);
+    }
+
+    @PutMapping("{id}")
+    public Libro actualizarLibro(@PathVariable int id, @RequestBody Libro libro) {
+        return libroService.actualizarLibro(libro);
+    }
+
+    @DeleteMapping
+    public String eliminarLibro(@PathVariable int id){
+        return libroService.eliminarLibro(id);
+    }
+
+
+
+
+
+
+
 
 
 }
